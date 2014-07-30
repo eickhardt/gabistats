e612b3e042407f70aeb91cdd7ecbc9eca6184a94
Daniel Eickhardt <ddeickhardt@hotmail.com>
2014-07-30 23:51:48 +0200

    Trying again

--- app/config/database.php
+++ app/config/database.php
@@ -1,7 +1,7 @@
 <?php
 
 // Added by Daniel to easily allow for different environment settings
-@require_once(__DIR__."/localsettings.php");
+@require_once(base_path()."localsettings.php");
 
 return array(
 
